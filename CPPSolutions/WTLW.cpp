20a6678fab32a766923c9dab662310b3d6b5094e
dilbarjot <idilbarrandhawa@gmail.com>
2021-01-04 11:56:17 -0600

    sol to 4A & 71A

---
+++ CPPSolutions/WTLW.cpp
@@ -0,0 +1,26 @@
+/**
+ ** Created by legionary on 2021-01-04
+ ** https://codeforces.com/problemset/problem/71/A
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string line;
+    int tt;
+    cin >> tt;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    while (tt) {
+        getline(cin, line);
+        int ln = line.length();
+        if (ln > 10) {
+            cout << line.at(0) << ln-2 << line.at(ln - 1) << "\n";
+        } else cout << line << "\n";
+        tt--;
+    }
+    return 0;
+}
---
+++ CPPSolutions/Watermelon.cpp
@@ -0,0 +1,21 @@
+/**
+ ** Created by legionary on 2021-01-04
+ ** https://codeforces.com/problemset/problem/4/A
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    if(n>2){
+        if(n%2==0){
+            cout << "YES\n";
+        }else cout << "NO\n";
+    }else cout << "NO\n";
+    return 0;
+}

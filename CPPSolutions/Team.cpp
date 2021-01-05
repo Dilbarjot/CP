4d83793e4f56b1f03f22a678567f5c1aff5d5077
dilbarjot <idilbarrandhawa@gmail.com>
2021-01-04 23:29:59 -0600

    231A sol

---
+++ CPPSolutions/Team.cpp
@@ -0,0 +1,26 @@
+/**
+ ** Created by legionary on 2021-01-04
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt;
+    cin >> tt;
+    int solved = 0;
+    while (tt) {
+        int p, v, t, sum = 0;
+        cin >> p >> v >> t;
+        sum = p + v + t;
+        if (sum >= 2) {
+            solved++;
+        }
+        tt--;
+    }
+    cout << solved << "\n";
+    return 0;
+}
